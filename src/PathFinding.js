8aacc1cf0da8ddb2a215a5064ab4bd7927d1ca2e
Kriti Raj <63531465+kritir@users.noreply.github.com>
2020-07-24 17:20:27 +0530

    Create PathFinding.js

---
+++ src/PathFinding.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'Heap'                      : require('heap'),
+    'Node'                      : require('./core/Node'),
+    'Grid'                      : require('./core/Grid'),
+    'Util'                      : require('./core/Util'),
+    'DiagonalMovement'          : require('./core/DiagonalMovement'),
+    'Heuristic'                 : require('./core/Heuristic'),
+		'BreadthFirstFinder'        : require('./finders/BreadthFirstFinder'),
+    'DijkstraFinder'            : require('./finders/DijkstraFinder'),
+		'BiBreadthFirstFinder'      : require('./finders/BiBreadthFirstFinder'),
+    'BiDijkstraFinder'          : require('./finders/BiDijkstraFinder'),
+};
